2690c44c8d4b51805aeaefd0bb414f6099f4d39a
Kotovich <rggb@icomp.ufam.edu.br>
2018-05-16 08:48:21 -0400

    [INS] PhP1 exercise

---
+++ PHP1/exercicio1.php
@@ -0,0 +1,7 @@
+<?php
+echo "Postado</br></br>";
+echo "Nome : ".$_POST["nome"]."</br>";
+echo "Email : ".$_POST['email']."</br>";
+echo "Site : ".$_POST['site']."</br></br>";
+echo "Mensagem : ".$_POST['mensagem']."</br>";
+?>
