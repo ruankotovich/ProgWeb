e50dc3b67999d9b3b741ecf2d8fdc44aa1e5fd79
Unknown <rggb@icomp.ufam.edu.br>
2018-07-02 21:33:53 -0400

    [INS] Exercises part 7 (layout update)

--- Yii/advanced/frontend/views/layouts/main.php
+++ Yii/advanced/frontend/views/layouts/main.php
@@ -37,11 +37,13 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Sobre', 'url' => ['/site/about']],
+        ['label' => 'Contato', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => 'Cursos', 'url' => ['/curso/index']];
+        $menuItems[] = ['label' => 'Usuários', 'url' => ['/user/index']];
+        $menuItems[] = ['label' => 'Cadastro', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
