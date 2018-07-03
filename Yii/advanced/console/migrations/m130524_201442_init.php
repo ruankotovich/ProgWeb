dce25e1b2e9f04a8d7a9f5d095edc496729766cd
Unknown <rggb@icomp.ufam.edu.br>
2018-07-02 20:27:29 -0400

    [INS] Yii Exercises part 3

--- Yii/advanced/console/migrations/m130524_201442_init.php
+++ Yii/advanced/console/migrations/m130524_201442_init.php
@@ -13,17 +13,51 @@ class m130524_201442_init extends Migration
         }
 
         $this->createTable('{{%user}}', [
+            // 'id' => $this->integer()->primaryKey(),
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
+            'id_curso' => $this->integer()->notNull(),                        
 
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->createTable('curso',[
+            'id' => $this->primaryKey(),
+            'nome' => $this->string(45)->notNull()->unique(),
+            'sigla' => $this->string(4)->notNull(),
+            'descricao' => $this->string()
+        ],$tableOptions);
+
+        $this->createTable('jogada',[
+            'id' => $this->primaryKey(),
+            'id_user' => $this->integer()->notNull(),
+            'pontuacao' => $this->integer()->notNull(),
+            'data_hora' => $this->string(50)->notNull()
+        ],$tableOptions);
+
+        $this->addForeignKey(
+            'fk-user-curso',
+            '{{%user}}',
+            'id_curso',
+            'curso',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-jogada-user',
+            'jogada',
+            'id_user',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
     }
 
     public function down()
