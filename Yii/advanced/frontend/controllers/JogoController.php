4cd0f5515c52903de7904cd69f72e5f8bbf5f333
Unknown <rggb@icomp.ufam.edu.br>
2018-07-03 17:23:04 -0400

    [INS] Ajax requisition and rank save

--- Yii/advanced/frontend/controllers/JogoController.php
+++ Yii/advanced/frontend/controllers/JogoController.php
@@ -2,8 +2,10 @@
 
 namespace frontend\controllers;
 
+use common\models\Jogada;
 use Yii;
 use yii\web\Controller;
+
 /**
  * JogoController implements the CRUD actions for Jogada model.
  */
@@ -13,14 +15,25 @@ class JogoController extends Controller
     {
         return $this->render('index', [
             'user' => Yii::$app->user->identity,
-            'isGuest' => Yii::$app->user->isGuest
+            'isGuest' => Yii::$app->user->isGuest,
+            'ranking' => $this->getRanking(3),
         ]);
     }
-    public function actionSave($pontuacao){
-        return $this->render('index',[
-            'user' => Yii::$app->user->identity,
-            'isGuest' => Yii::$app->user->isGuest,
-            'pontuacao'=>$pontuacao
-        ]);
+    public function actionSave($pontuacao, $userId)
+    {
+        $round = new Jogada();
+        $round->id_user = $userId;
+        $round->pontuacao = $pontuacao;
+        $round->data_hora = date("d/m/Y H:i:s");
+        $round->save();
+    }
+
+    public function getRanking($limit)
+    {
+        if ($limit > 0) {
+            return Jogada::find()->with('user')->orderBy('pontuacao desc')->limit($limit)->all();
+        } else {
+            return Jogada::find()->with('user')->orderBy('pontuacao desc')->all();
+        }
     }
 }
--- Yii/advanced/frontend/views/jogo/index.php
+++ Yii/advanced/frontend/views/jogo/index.php
@@ -18,15 +18,17 @@ $this->title = 'Yiieti';
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-const URL = "<?= Url::to(['jogo/save']) ?>";
 
+<script>
+const URL = "<?=Url::to(['jogo/save'])?>";
+const USER_ID = "<?= $isGuest?0:$user->id ?>"
     savePuntuaction = (pt) => {
       $.ajax({
         type: 'GET',
         url: URL,
         data: {
-          'pontuacao': pt
+          'pontuacao': pt,
+          'userId': USER_ID?>,
         },
         error: function () {
           console.log('Deu algum erro!');
@@ -37,31 +39,42 @@ const URL = "<?= Url::to(['jogo/save']) ?>";
       });
     }
 </script>
+
 <body>
 <?php
-    if(!$isGuest):
+if (!$isGuest):
 ?>
 <div style="position:absolute">
-    <h1>Прибет, <?=$user->username ?>!</h1>
-    <?php if(!isset($pontuacao)){$pontuacao = 0;} ?>
-    Ты пунтуация это <?= $pontuacao ?>
+    <h1>Прибет, <?=$user->username?>!</h1>
+    <?php if (!isset($pontuacao)) {$pontuacao = 0;}?>
     <div id="montanha">
         <div id="skier"></div>
     </div>
-    <div id="panel">
+    <div class="panel" id="panel">
     </div>
-    <!-- <script src="js/skifree.js"></script> -->
-   
-   <?php $this->registerJsFile('js/skifree.js'); ?>
+    <div class="panel" id="ranking">
+    <center><b>Ranking</b></center>
+
+    <?php
+    $iterator = 1;
+        foreach($ranking as $item){
+            $username = $item->user->username;
+            echo "<div style='vertical-aligh:middle'><img src='gfx/medal_$iterator.png' width='35em'/>&nbsp;<font color='red'>$iterator º &nbsp;&nbsp;</font> $username - $item->pontuacao<br></div>";
+            $iterator++;
+        }
+    ?>
+
+    </div>
    </div>
+   <?php $this->registerJsFile('js/skifree.js');?>
 <?php
-    else:
+else:
 ?>
 <center><h1><font color='red'>ты не вошел!</font></h1>
-<?= Html::a('Логин', ['/site/login'], ['class' => 'btn btn-primary']) ?>
+<?=Html::a('Логин', ['/site/login'], ['class' => 'btn btn-primary'])?>
 </center>
 <?php
-    endif;
+endif;
 ?>
 </body>
 
